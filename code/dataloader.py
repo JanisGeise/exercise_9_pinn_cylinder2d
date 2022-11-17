implemented scaling of data & bug fixes

code/dataloader.py
@@ -29,11 +29,11 @@ def export_cylinder2D_flow_field(load_path: str, save_path: str) -> None:
     """
     # load the snapshots for t < 6...8 s, because N_time_steps need to be dividable by ratio for batch size
     loader = FOAMDataloader(load_path)
-    t = [time for time in loader.write_times if float(time) > 5.0]
+    t = [time for time in loader.write_times if float(time) > 7.0]
 
     # only read in [u, v] of U-field and discard the w-component, since flow is 2D, also ignore major parts of the wake
     # for testing purposes: discard cylinder, since mesh is very fine there
-    mask = mask_box(loader.vertices[:, :2], lower=[0.25, -1], upper=[0.75, 1])
+    mask = mask_box(loader.vertices[:, :2], lower=[0.35, -1], upper=[1.0, 1])
     u_field = pt.zeros((mask.sum().item(), len(t)), dtype=pt.float32)
     v_field = pt.zeros((mask.sum().item(), len(t)), dtype=pt.float32)
     p_field = pt.zeros((mask.sum().item(), len(t)), dtype=pt.float32)
@@ -69,7 +69,8 @@ def export_cylinder2D_flow_field(load_path: str, save_path: str) -> None:
         pickle.dump(data_out, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-def read_data(file: str, portion: Union[float, int], n_dims: int = 2, u_infty: int = 1, d: float = 0.1, mu: float = 1e-3):
+def read_data(file: str, portion: Union[float, int], n_dims: int = 2, u_infty: int = 1, d: float = 0.1,
+              mu: float = 1e-3, scale: bool=True):
     """
     reads in the flow field data from CFD
 
@@ -80,6 +81,7 @@ def read_data(file: str, portion: Union[float, int], n_dims: int = 2, u_infty: i
     :param u_infty: free stream velocity at inlet
     :param d: diameter of cylinder
     :param mu: kinematic viscosity
+    :param scale: flag if loaded data should be scaled to [0, 1]
     :return:
     """
     # read flow field data and non-dimensionalize it
@@ -89,19 +91,29 @@ def read_data(file: str, portion: Union[float, int], n_dims: int = 2, u_infty: i
     T_star = data_mat["t"] / (d * u_infty)
     P_star = data_mat["p"] * d / (mu * u_infty)
 
-    """ not working...
-    V_star, min_max_v = scale_data(U_star[:, 1, :])
-    U_star, min_max_u = scale_data(U_star[:, 0, :])
-    Y_star, min_max_y = scale_data(X_star[:, 1].unsqueeze(-1))
-    X_star, min_max_x = scale_data(X_star[:, 0].unsqueeze(-1))
-    T_star, min_max_t = scale_data(T_star)
-    P_star, min_max_p = scale_data(P_star)
-    """
+    # scale all available CFD data to interval [0, 1]
+    if scale:
+        V_star, min_max_v = scale_data(U_star[:, 1, :])
+        U_star, min_max_u = scale_data(U_star[:, 0, :])
+        Y_star, min_max_y = scale_data(X_star[:, 1].unsqueeze(-1))
+        X_star, min_max_x = scale_data(X_star[:, 0].unsqueeze(-1))
+        T_star, min_max_t = scale_data(T_star)
+        P_star, min_max_p = scale_data(P_star)
 
-    V_star = U_star[:, 1, :]
-    U_star = U_star[:, 0, :]
-    Y_star = X_star[:, 1]
-    X_star = X_star[:, 0]
+        # feature matrix contains min- / max-values as well, but for all loaded data points
+        min_max_vals = {"u": min_max_u, "v": min_max_v, "x": min_max_x, "y": min_max_y, "p": min_max_p, "t": min_max_t}
+
+        # sanity check -> if not enough data points, then min == max and scaling returns 'nan'
+        for key in min_max_vals:
+            if abs(min_max_vals[key][0] - min_max_vals[key][1]).item() <= 10e-6:
+                print("not enough values for normalization, increase 'ratio'!")
+                exit()
+    else:
+        V_star = U_star[:, 1, :]
+        U_star = U_star[:, 0, :]
+        Y_star = X_star[:, 1]
+        X_star = X_star[:, 0]
+        min_max_vals = {}
 
     # Read the number of coordinate points N and the number of time steps T
     N = X_star.shape[0]
@@ -118,12 +130,8 @@ def read_data(file: str, portion: Union[float, int], n_dims: int = 2, u_infty: i
     u = U_star.flatten()[:, None]
     v = V_star.flatten()[:, None]
     p = P_star.flatten()[:, None]
-    temp = np.concatenate((x, y, t, u, v, p), 1)
 
     # shape = N_dimensions * N_parameters (parameters = x, y, t, u, v, p)
-    feature_mat = np.empty((n_dims, 6))
-    feature_mat[0, :] = np.max(temp, 0)
-    feature_mat[1, :] = np.min(temp, 0)
     x_unique = np.unique(x).reshape(-1, 1)
     y_unique = np.unique(y).reshape(-1, 1)
     index_arr_x = np.linspace(0, len(x_unique) - 1, int(len(x_unique) * portion)).astype(int).reshape(-1, 1)
@@ -131,6 +139,7 @@ def read_data(file: str, portion: Union[float, int], n_dims: int = 2, u_infty: i
     x_select = x_unique[index_arr_x].reshape(-1, 1)
     y_select = y_unique[index_arr_y].reshape(-1, 1)
 
+    # reconstruct the flow fields for the given (N*1D) data to 2D-mesh for each parameter
     index_x = np.empty((0, 1), dtype=int)
     index_y = np.empty((0, 1), dtype=int)
     for select_1 in x_select:
@@ -147,11 +156,21 @@ def read_data(file: str, portion: Union[float, int], n_dims: int = 2, u_infty: i
     x = pt.tensor(x, dtype=pt.float32)
     y = pt.tensor(y, dtype=pt.float32)
     t = pt.tensor(t, dtype=pt.float32)
-    feature_mat = pt.tensor(feature_mat, dtype=pt.float32)
-    return x, y, t, u, v, p, feature_mat
+
+    return x, y, t, u, v, p, min_max_vals
 
 
-def generate_eqp_rect(low_bound, up_bound, dimension, points):
+def generate_eqp_rect(low_bound: Union[pt.Tensor, np.ndarray], up_bound: Union[pt.Tensor, np.ndarray], dimension: int,
+                      points: int) -> pt.Tensor:
+    """
+    creates mesh for given number of points and dimensions for solving the NS-equations
+
+    :param low_bound: min. value for each dimension (including temporal dimension)
+    :param up_bound: max. value for each dimension (including temporal dimension)
+    :param dimension: number of spacial and temporal dimensions
+    :param points: number of grid points for solving the NS-equations
+    :return: mesh with grid points for solving the NS-equations
+    """
     # Generate rectangular domain equation points
     eqa_xyzt = low_bound + (up_bound - low_bound) * lhs(dimension, points)
     per = np.random.permutation(eqa_xyzt.shape[0])
@@ -160,33 +179,35 @@ def generate_eqp_rect(low_bound, up_bound, dimension, points):
     return eqa_points
 
 
-def prepare_data(x: pt.Tensor, y: pt.Tensor, t: pt.Tensor, u: pt.Tensor, v: pt.Tensor, p: pt.Tensor, loaded_data,
-                 n_eq_points: int = 10000, dimensions: int = 3,
-                 batch_ratio: float = 0.005):
+def prepare_data(x: pt.Tensor, y: pt.Tensor, t: pt.Tensor, u: pt.Tensor, v: pt.Tensor, p: pt.Tensor,
+                 n_eq_points: int = 10000, dimensions: int = 3, batch_ratio: float = 0.005) -> dict:
     """
     TODO
 
-    :param x:
-    :param y:
-    :param t:
-    :param u:
-    :param v:
-    :param p:
-    :param loaded_data:
-    :param n_eq_points:
+    :param x: x-coordinates
+    :param y: y-coordinates
+    :param t: all available time steps
+    :param u: data of velocity field for u (for all available time steps)
+    :param v: data of velocity field for v (for all available time steps)
+    :param p: data of pressure field (for all available time steps)
+    :param loaded_data: TODO
+    :param n_eq_points: N grid points for solving NS-equations
     :param dimensions: spacial + temporal dimensions
-    :param batch_ratio:
-    :return:
+    :param batch_ratio: TODO
+    :return: TODO
     """
     x_random = shuffle_data(x, y, t, u, v, p)
-    lb = np.array([loaded_data.data.numpy()[1, 0], loaded_data.data.numpy()[1, 1], loaded_data.data.numpy()[1, 2]])
-    ub = np.array([loaded_data.data.numpy()[0, 0], loaded_data.data.numpy()[0, 1], loaded_data.data.numpy()[0, 2]])
-    points_eq = generate_eqp_rect(lb, ub, dimensions, n_eq_points)
+
+    # since data is normalized to [0, 1], the bounds for each parameter within parameter space is [0, 1]
+    points_eq = generate_eqp_rect(np.zeros(3), np.ones(3), dimensions, n_eq_points)
     batch_size_data = int(batch_ratio * x_random.shape[0])
 
+    if batch_size_data == 0:
+        print("not enough data points, increase the 'ratio' parameter!")
+        exit()
     return {"points_eq": points_eq, "batch_size_data": batch_size_data, "x_random": x_random,
-            "eq_iter": int(points_eq.size(0) / int(batch_ratio * points_eq.shape[0])),
-            "batch_size_eq": int(batch_ratio * points_eq.shape[0]), "inner_iter": int(x_random.size(0) / batch_size_data)}
+            "eq_iter": int(points_eq.size()[0] / int(batch_ratio * points_eq.shape[0])),
+            "batch_size_eq": int(batch_ratio*points_eq.shape[0]), "inner_iter": int(x_random.size()[0]/batch_size_data)}
 
 
 def shuffle_data(x, y, t, u, v, p):

code/post_process_results.py
@@ -13,7 +13,7 @@ from pandas import read_csv
 from typing import Union, Tuple
 import matplotlib.pyplot as plt
 
-from dataloader import read_data
+from dataloader import read_data, rescale_data
 from train_models import PINN
 from train_models import f_equation_inverse
 
@@ -46,7 +46,7 @@ def plot_loss(path: str, name_loss_file: str = "loss") -> None:
     plt.close("all")
 
 
-def compare_at_select_time_series(path: str, file_name: str, lower_time: Union[float, int] = 4.0,
+def compare_at_select_time_series(path: str, file_name: str, min_max: dict, lower_time: Union[float, int] = 4.0,
                                   upper_time: Union[float, int] = 8.0, d: float = 0.1, mu: float = 1e-3,
                                   u_infty: Union[int, float] = 1, model_name: str = "model_train") -> None:
     """
@@ -54,6 +54,7 @@ def compare_at_select_time_series(path: str, file_name: str, lower_time: Union[f
 
     :param path: save path for plots
     :param file_name: name of the file containing the real flow field data
+    :param min_max: min- / max-values for scaling the loaded data
     :param lower_time: starting time for prediction
     :param upper_time: end time for prediction
     :param model_name: name of the trained model
@@ -62,21 +63,18 @@ def compare_at_select_time_series(path: str, file_name: str, lower_time: Union[f
     :param u_infty: free stream velocity at inlet
     :return: None
     """
-    # read in the data and model, TODO: everything here is still quite inefficient...
+    # read in the data
+    if min_max is None:
+        min_max = {}
     xy_cfd = pickle.load(open(file_name, "rb"))["xy_coord"]
     uv_cfd = pickle.load(open(file_name, "rb"))["U"]
     p_cfd = pickle.load(open(file_name, "rb"))["p"]
+    x, y, t, u, v, p, _ = read_data(file_name, 1, scale=False)
 
-    x, y, t, u, v, p, _ = read_data(file_name, 1)
-    data_stack = np.concatenate((x, y, t, u, v, p), axis=1)
+    # read in the best model
     pinn_net = PINN()
     pinn_net.load_state_dict(pt.load("".join([path, model_name, ".pt"]), map_location="cpu"))
     pinn_net.eval()
-
-    # min-max scaling for plots
-    min_data = np.min(data_stack, 0).reshape(1, data_stack.shape[1])
-    max_data = np.max(data_stack, 0).reshape(1, data_stack.shape[1])
-    del data_stack, u, v, p
 
     # Keep the coordinates that are not repeated in the data set
     x = np.unique(x).reshape(-1, 1)
@@ -100,25 +98,32 @@ def compare_at_select_time_series(path: str, file_name: str, lower_time: Union[f
 
         # predict the flow field of the current time step
         u_predict, v_predict, p_predict = f_equation_inverse(x_selected, y_selected, t_selected, pinn_net.eval())
+
+        # reshape & re-scale the flow fields
+        u_predict = rescale_data(u_predict, min_max["u"])
+        v_predict = rescale_data(v_predict, min_max["v"])
+        p_predict = rescale_data(p_predict, min_max["p"])
         u_predict = u_predict.data.numpy().reshape(mesh_x.shape)
         v_predict = v_predict.data.numpy().reshape(mesh_x.shape)
         p_predict = p_predict.data.numpy().reshape(mesh_x.shape)
 
         # plot the velocity- and pressure field for each given time step
         plot_comparison_flow_field(path, [xy_cfd[:, 0], xy_cfd[:, 1], uv_cfd[:, 0, idx]*u_infty], u_predict*u_infty,
-                                   select_time, name="u", min_value=min_data[0, 3], max_value=max_data[0, 3])
+                                   select_time, name="u", min_value=min_max["u"][0], max_value=min_max["u"][1])
         plot_comparison_flow_field(path, [xy_cfd[:, 0], xy_cfd[:, 1], uv_cfd[:, 1, idx]*u_infty], v_predict*u_infty,
-                                   select_time, name="v", min_value=min_data[0, 4], max_value=max_data[0, 4])
+                                   select_time, name="v", min_value=min_max["v"][0], max_value=min_max["v"][1])
         plot_comparison_flow_field(path, [xy_cfd[:, 0], xy_cfd[:, 1], p_cfd[:, idx] * (mu * u_infty) / d],
-                                   p_predict * (mu * u_infty) / d, select_time, name="p", min_value=min_data[0, 5],
-                                   max_value=max_data[0, 5])
+                                   p_predict * (mu * u_infty) / d, select_time, name="p", min_value=min_max["p"][0],
+                                   max_value=min_max["p"][1])
 
         # free up some memory
         del u_predict, v_predict, p_predict, x_selected, y_selected, t_selected, x_flatten, t_flatten
+        exit()
 
 
 def plot_comparison_flow_field(path: str, q_selected, q_predict, select_time, min_value, max_value, name="u") -> None:
     """
+    plot the real flow field from CFD in comparison to the predicted one
 
     :param path: save path for plots
     :param q_selected: CFD field data
@@ -134,14 +139,19 @@ def plot_comparison_flow_field(path: str, q_selected, q_predict, select_time, mi
     for i in range(2):
         if i == 0:
             ax[i].set_title(f"$Real$ $flow$ $field$ $at$ $t = " + "{:.2f}s$".format(select_time), usetex=True)
-            # TODO: norm doesn't have any effect, bounds of color bar changing every time step...
-            real = ax[i].tricontourf(q_selected[0], q_selected[1], q_selected[2], levels=200, cmap="jet", norm=v_norm)
-            cb = fig.colorbar(real, ax=ax[i], format="{x:.2f}", norm=v_norm)
+            if name == "p":
+                real = ax[i].tricontourf(q_selected[0], q_selected[1], q_selected[2], levels=200, cmap="jet")
+            else:
+                real = ax[i].tricontourf(q_selected[0], q_selected[1], q_selected[2], levels=200, cmap="jet",
+                                         norm=v_norm)
+            cb = fig.colorbar(real, ax=ax[i], format="{x:.2f}")
         else:
             ax[i].set_title(f"$Predicted$ $flow$ $field$ $at$ $t = " + "{:.2f}s$".format(select_time), usetex=True)
-            # TODO: norm doesn't have any effect, bounds of color bar changing every time step...
-            pred = ax[i].contourf(q_predict, levels=200, cmap="jet", norm=v_norm)
-            cb = fig.colorbar(pred, ax=ax[i], format="{x:.2f}", norm=v_norm)
+            if name == "p":
+                pred = ax[i].contourf(q_predict, levels=200, cmap="jet")
+            else:
+                pred = ax[i].contourf(q_predict, levels=200, cmap="jet", norm=v_norm)
+            cb = fig.colorbar(pred, ax=ax[i], format="{x:.2f}")
             # TODO: this gives a warning from plt
             ax[i].set_yticklabels(["{:.2f}".format(t) for t in ax[0].get_yticks()])
             ax[i].set_xticklabels(["{:.2f}".format(t) for t in ax[0].get_xticks()])

code/train_models.py
@@ -18,7 +18,7 @@ class PINN(nn.Module):
     """
     this class implements a PINN
     """
-    def __init__(self, n_inputs: int = 3, n_outputs: int = 2, n_layers: int = 8, n_neurons: int = 25,
+    def __init__(self, n_inputs: int = 3, n_outputs: int = 2, n_layers: int = 15, n_neurons: int = 50,
                  activation: callable = pt.nn.ReLU()):
         super(PINN, self).__init__()
         self.n_inputs = n_inputs
@@ -31,16 +31,17 @@ class PINN(nn.Module):
         # input layer to first hidden layer
         self.base.add_module("0linear", nn.Linear(self.n_inputs, n_neurons))
         self.base.add_module("0Act", self.activation)
-        # self.base.add_module("0LayerNorm", nn.InstanceNorm1d(num_features=self.n_neurons))
+        # self.base.add_module("0BatchNorm1d", nn.BatchNorm1d(self.n_neurons))
 
         # add more hidden layers if specified
         if self.n_layers > 1:
             for i in range(1, self.n_layers - 1):
-                self.base.add_module(str(i) + "linear", nn.Linear(self.n_neurons, self.n_neurons))
                 self.base.add_module(str(i) + "Act", self.activation)
-                # self.base.add_module(str(i) + "LayerNorm", nn.InstanceNorm1d(num_features=self.n_neurons))
+                self.base.add_module(str(i) + "linear", nn.Linear(self.n_neurons, self.n_neurons))
+                # self.base.add_module(str(i) + "BatchNorm1d", nn.BatchNorm1d(self.n_neurons))
 
         # last hidden layer to output
+        # self.base.add_module(str(self.n_layers) + "BatchNorm1d", nn.BatchNorm1d(self.n_neurons))
         self.base.add_module(str(self.n_layers) + "linear", nn.Linear(self.n_neurons, self.n_outputs))
         self.lam1 = nn.Parameter(pt.randn(1, requires_grad=True))
         self.lam2 = nn.Parameter(pt.randn(1, requires_grad=True))
@@ -54,9 +55,9 @@ class PINN(nn.Module):
     def initial_param(self):
         # initialize the weights and biases of each layer
         for name, param in self.base.named_parameters():
-            if name.endswith("weight") and not name.startswith("LayerNorm", 1):
+            if name.endswith("weight") and not name.startswith("BatchNorm1d", 1):
                 nn.init.xavier_normal_(param)
-            elif name.endswith("bias") and not name.startswith("LayerNorm", 1):
+            elif name.endswith("bias") and not name.startswith("BatchNorm1d", 1):
                 nn.init.zeros_(param)
 
     def data_mse(self, x, y, t, u, v, p):
@@ -146,7 +147,7 @@ def train_pinns(inner_iter: int, x_random: pt.Tensor, batch_size_data: int, batc
     best_loss = 1.0e5
     pinn_net = PINN()
     losses = np.empty((0, 3), dtype=float)
-    optimizer = pt.optim.AdamW(pinn_net.parameters(), lr=0.01, weight_decay=10e-3)
+    optimizer = pt.optim.AdamW(pinn_net.parameters(), lr=5e-4, weight_decay=10e-3)
 
     # start timer
     start = time()